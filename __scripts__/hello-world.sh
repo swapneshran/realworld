de1834a4c4257c8d6e57b1cf65290e3d2b1cd712
swapneshran <swapneshmohanty9220@gmail.com>
2020-07-12 13:37:36 +0100

    adding through yml

--- __scripts__/hello-world.sh
+++ __scripts__/hello-world.sh
@@ -1,22 +1,1 @@
-#!/bin/bash
-
-# pass the previous step output to script
-
-echo "getting Log URL"
-echo "ACTION_ID: $GITHUB_ACTION"
-echo "$GITHUB_TOKEN"
-
-SHA_ID=$(curl -X  GET  https://api.github.com/repos/swapneshran/realworld/actions/runs  -H 'Authorization: token ${{ secrets.GITHUB_TOKEN }}' \
-                    -H "Accept: application/vnd.github.v3+json" | jq '.workflow_runs[0].head_sha' | sed 's/"//g' ) \
-
-
-echo "work flow id $SHA_ID"
-
-
-SHA_ID2=$(curl -X  GET  https://api.github.com/repos/swapneshran/realworld/commits/$SHA_ID/check-runs -H 'Authorization: token ${{ secrets.GITHUB_TOKEN }}' \
-                     -H "Accept: application/vnd.github.antiope-preview+json" | jq '.check_runs[0].id' )
-
-echo "Check suite id is $SHA_ID2"
-LOG_URL=("https://github.com/swapneshran/realworld/commit/$SHA_ID/checks/$SHA_ID2/logs")
-echo $LOG_URL
-echo "##[set-output name=log_url;]$LOG_URL"
+#!/bin/bash# pass the previous step output to scriptecho "getting Log URL"echo "ACTION_ID: $GITHUB_ACTION"echo "$GITHUB_TOKEN"SHA_ID=$(curl -X  GET  https://api.github.com/repos/swapneshran/realworld/actions/runs  -H 'Authorization: token ${{ secrets.GITHUB_TOKEN }}' \                    -H "Accept: application/vnd.github.v3+json" | jq '.workflow_runs[0].head_sha' | sed 's/"//g' ) \echo "work flow id $SHA_ID"SHA_ID2=$(curl -X  GET  https://api.github.com/repos/swapneshran/realworld/commits/$SHA_ID/check-runs -H 'Authorization: token ${{ secrets.GITHUB_TOKEN }}' \                     -H "Accept: application/vnd.github.antiope-preview+json" | jq '.check_runs[0].id' )echo "Check suite id is $SHA_ID2"LOG_URL=("https://github.com/swapneshran/realworld/commit/$SHA_ID/checks/$SHA_ID2/logs")echo $LOG_URLecho "##[set-output name=log_url;]$LOG_URL"
