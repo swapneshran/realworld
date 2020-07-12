9f2d74eb3f71613e759d3bc8b0477625610ea88e
swapneshran <swapneshmohanty9220@gmail.com>
2020-07-12 01:07:20 +0100

    silent npm output

--- __scripts__/hello-world.sh
+++ __scripts__/hello-world.sh
@@ -3,9 +3,12 @@
 # pass the previous step output to script
 
 echo "Hello World"
-echo "running npm install"
+echo "running npm install --silent"
 
 npm install 2>&1 | tee log.txt
+
+
+echo "printing the install script"
 
 echo "$(cat log.txt)"
 
