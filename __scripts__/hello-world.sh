fb69f810e7aae0917400cbcb9cf76280139354d1
swapneshran <swapneshmohanty9220@gmail.com>
2020-07-12 13:31:39 +0100

    adding through shell file

--- __scripts__/hello-world.sh
+++ __scripts__/hello-world.sh
@@ -2,21 +2,24 @@
 
 # pass the previous step output to script
 
-echo "Hello World"
-echo "running npm install --silent"
+echo "getting Log URL"
+echo "ACTION_ID: $GITHUB_ACTION"
+echo "$GITHUB_TOKEN"
 
-npm install 2>&1 | tee -a log.txt
-
-
-echo "printing the install script"
-
-echo "printing the install script"
+SHA_ID=$(curl -X  GET  https://api.github.com/repos/swapneshran/realworld/actions/runs  -H 'Authorization: token ${{ secrets.GITHUB_TOKEN }}' \
+                    -H "Accept: application/vnd.github.v3+json" | jq '.workflow_runs[0].head_sha' \
+                     | sed 's/"//g' ) \
 
 
+echo "work flow id $SHA_ID"
 
-echo "printing the install script"
 
-echo "running build"
+SHA_ID2=$(curl -X  GET  https://api.github.com/repos/swapneshran/realworld/commits/$SHA_ID/check-runs -H 'Authorization: token ${{ secrets.GITHUB_TOKEN }}' \
+                     -H "Accept: application/vnd.github.antiope-preview+json" \
+                      | jq '.check_runs[0].id'
+                      )
 
-npm run build | tee -a log.txt
-
+echo "Check suite id is $SHA_ID2"
+LOG_URL=("https://github.com/swapneshran/realworld/commit/$SHA_ID/checks/$SHA_ID2/logs")
+echo $LOG_URL
+echo "##[set-output name=log_url;]$LOG_URL"
