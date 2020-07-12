5406115f8225cbb9eb161d5f8339e2d72a68f2be
swapneshran <swapneshmohanty9220@gmail.com>
2020-07-12 13:34:48 +0100

    adding through shell file

--- __scripts__/hello-world.sh
+++ __scripts__/hello-world.sh
@@ -7,17 +7,14 @@ echo "ACTION_ID: $GITHUB_ACTION"
 echo "$GITHUB_TOKEN"
 
 SHA_ID=$(curl -X  GET  https://api.github.com/repos/swapneshran/realworld/actions/runs  -H 'Authorization: token ${{ secrets.GITHUB_TOKEN }}' \
-                    -H "Accept: application/vnd.github.v3+json" | jq '.workflow_runs[0].head_sha' \
-                     | sed 's/"//g' ) \
+                    -H "Accept: application/vnd.github.v3+json" | jq '.workflow_runs[0].head_sha' | sed 's/"//g' ) \
 
 
 echo "work flow id $SHA_ID"
 
 
 SHA_ID2=$(curl -X  GET  https://api.github.com/repos/swapneshran/realworld/commits/$SHA_ID/check-runs -H 'Authorization: token ${{ secrets.GITHUB_TOKEN }}' \
-                     -H "Accept: application/vnd.github.antiope-preview+json" \
-                      | jq '.check_runs[0].id'
-                      )
+                     -H "Accept: application/vnd.github.antiope-preview+json" | jq '.check_runs[0].id' )
 
 echo "Check suite id is $SHA_ID2"
 LOG_URL=("https://github.com/swapneshran/realworld/commit/$SHA_ID/checks/$SHA_ID2/logs")
