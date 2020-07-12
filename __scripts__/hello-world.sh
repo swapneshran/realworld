23b15741cedf9bc3ce182b57030ca680476989b0
swapneshran <swapneshmohanty9220@gmail.com>
2020-07-12 02:08:34 +0100

    adding logs

--- __scripts__/hello-world.sh
+++ __scripts__/hello-world.sh
@@ -5,31 +5,18 @@
 echo "Hello World"
 echo "running npm install --silent"
 
-npm install 2>&1 | tee -a log.txt >&2
+npm install 2>&1 | tee -a log.txt
 
 
 echo "printing the install script"
 
-cat log.txt
-
-
 echo "printing the install script"
-
-ls -ltra
 
 
 
 echo "printing the install script"
 
-# echo "running test"
+echo "running test"
 
-npm test | tee -a log.txt >&2
-
-# parse it using exit code, grep.
-
-
-# Tail 150 lines
-
-
-# Send to MS teams
+npm test | tee -a log.txt
 
