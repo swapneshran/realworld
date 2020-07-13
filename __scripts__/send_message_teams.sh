ee8403ceb2bf057b865a92b41a4e2210d207a3d5
swapneshran <swapneshmohanty9220@gmail.com>
2020-07-13 23:19:32 +0100

    adding shell script

--- __scripts__/send_message_teams.sh
+++ __scripts__/send_message_teams.sh
@@ -7,26 +7,28 @@
  BUILD_URL= BUILD_URL
  BUILD_STATUS= BUILD_STATUS
 
+ WEBHOOK_URL = 'https://webhook.site/ef9498ad-328a-4367-8d7d-3023268e7c00'
+
    if [ $NODE_SETUP_STATUS == 'failure' ]
    then
       echo "node set up is failed"
       MESSAGE='node set up is failed. for the branch'
       curl -H 'Content-Type: application/json' -d '{"text": '"$MESSAGE"'}' \
-      https://webhook.site/ef9498ad-328a-4367-8d7d-3023268e7c00
+        $WEBHOOK_URL
    elif [ $CYPRESS_STATUS == 'failure' ]
    then
       echo "cypress test is failed"
       MESSAGE='node set up is failed. for the branch'
       curl -H 'Content-Type: application/json' -d '{"text": '"$MESSAGE"'}' \
-      https://webhook.site/ef9498ad-328a-4367-8d7d-3023268e7c00
+       $WEBHOOK_URL
     elif [  $BUILD_STATUS == 'failure' ]
     then
        echo "build is failed"
        MESSAGE='node set up is failed. for the branch'
        curl -H 'Content-Type: application/json' -d '{"text": '"$MESSAGE"'}' \
-       https://webhook.site/ef9498ad-328a-4367-8d7d-3023268e7c00
+       $WEBHOOK_URL
      else
        echo "all the jobs are successful"
        curl -H 'Content-Type: application/json' -d '{"text": '"$MESSAGE"'}'  \
-                 https://webhook.site/ef9498ad-328a-4367-8d7d-3023268e7c00
+                 $WEBHOOK_URL
      fi
