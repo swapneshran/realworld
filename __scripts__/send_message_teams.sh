f4e0bba93bfbda3ac0f6890214d7a1e452018817
swapneshran <swapneshmohanty9220@gmail.com>
2020-07-13 23:15:24 +0100

    exceuting script

--- __scripts__/send_message_teams.sh
+++ __scripts__/send_message_teams.sh
@@ -1,25 +1,32 @@
 #!/usr/bin/env bash
 
-   if [ ${{needs.node-setup-lint-check.outputs.status}} == 'failure' ]
+ NODE_SETUP_LINT_URL= NODE_SETUP_LINT_URL
+ NODE_SETUP_STATUS= NODE_SETUP_STATUS
+ CYPRESS_LOG_URL=  CYPRESS_LOG_URL
+ CYPRESS_STATUS= CYPRESS_STATUS
+ BUILD_URL= BUILD_URL
+ BUILD_STATUS= BUILD_STATUS
+
+   if [ $NODE_SETUP_STATUS == 'failure' ]
    then
       echo "node set up is failed"
       MESSAGE='node set up is failed. for the branch'
-      curl -H 'Content-Type: application/json' -d '{"text": $MESSAGE}' \
+      curl -H 'Content-Type: application/json' -d '{"text": '"$MESSAGE"'}' \
       https://webhook.site/ef9498ad-328a-4367-8d7d-3023268e7c00
-   elif [ ${{needs.cypress-test.outputs.status}} == 'failure' ]
+   elif [ $CYPRESS_STATUS == 'failure' ]
    then
-          echo "cypress test is failed"
-             MESSAGE='node set up is failed. for the branch'
-             curl -H 'Content-Type: application/json' -d '{"text": $MESSAGE}' \
-             https://webhook.site/ef9498ad-328a-4367-8d7d-3023268e7c00
-          elif [ ${{needs.build.outputs.status}} == 'failure' ]
-          then
-             echo "build is failed"
-             MESSAGE='node set up is failed. for the branch'
-             curl -H 'Content-Type: application/json' -d '{"text": $MESSAGE}' \
-             https://webhook.site/ef9498ad-328a-4367-8d7d-3023268e7c00
-          else
-             echo "all the jobs are successful"
-             curl -H 'Content-Type: application/json' -d '{"text": $MESSAGE}' \
-                       https://webhook.site/ef9498ad-328a-4367-8d7d-3023268e7c00
-          fi
+      echo "cypress test is failed"
+      MESSAGE='node set up is failed. for the branch'
+      curl -H 'Content-Type: application/json' -d '{"text": '"$MESSAGE"'}' \
+      https://webhook.site/ef9498ad-328a-4367-8d7d-3023268e7c00
+    elif [  $BUILD_STATUS == 'failure' ]
+    then
+       echo "build is failed"
+       MESSAGE='node set up is failed. for the branch'
+       curl -H 'Content-Type: application/json' -d '{"text": '"$MESSAGE"'}' \
+       https://webhook.site/ef9498ad-328a-4367-8d7d-3023268e7c00
+     else
+       echo "all the jobs are successful"
+       curl -H 'Content-Type: application/json' -d '{"text": '"$MESSAGE"'}'  \
+                 https://webhook.site/ef9498ad-328a-4367-8d7d-3023268e7c00
+     fi
