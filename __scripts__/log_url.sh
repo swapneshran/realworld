602bd6e99917c5985462cf9d7fff00dd667a8116
swapneshran <swapneshmohanty9220@gmail.com>
2020-07-13 21:06:57 +0100

    adding git hub token

--- __scripts__/log_url.sh
+++ __scripts__/log_url.sh
@@ -1,16 +1,18 @@
+#!/usr/bin/env bash
 echo "Inside the script"
 
 echo $GITHUB_RUN_ID
 
+
 RUN_ID=$(curl -X  GET \
                                     https://api.github.com/repos/swapneshran/realworld/actions/runs \
-                                    -H 'Authorization: token ${{ secrets.GITHUB_TOKEN }}' \
+                                    -H "Authorization: token '$GITHUB_TOKEN'" \
                                     -H "Accept: application/vnd.github.v3+json" \
                                     | jq -r --arg GITHUB_RUN_ID "GITHUB_RUN_ID"  -c '.workflow_runs[] | select( .id | contains('$GITHUB_RUN_ID')) | .head_sha' \
                                     | sed 's/"//g')
 CHECK_SUITE_ID=$(curl -X  GET \
                                       https://api.github.com/repos/swapneshran/realworld/commits/$RUN_ID/check-runs \
-                                     -H 'Authorization: token ${{ secrets.GITHUB_TOKEN }}' \
+                                     -H "Authorization: token '$GITHUB_TOKEN'" \
                                      -H "Accept: application/vnd.github.antiope-preview+json" \
                                       | jq '.check_runs[0].id')
 LOG_URL=("https://github.com/repos/swapneshran/realworld/commit/$RUN_ID/checks/$CHECK_SUITE_ID/logs")
