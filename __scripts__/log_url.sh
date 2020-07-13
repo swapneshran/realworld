f9b096c72a1aa812f73e96c87c0d91a40da3149d
swapneshran <swapneshmohanty9220@gmail.com>
2020-07-13 21:18:21 +0100

    adding the shell script in every step

--- __scripts__/log_url.sh
+++ __scripts__/log_url.sh
@@ -1,21 +1,19 @@
 #!/usr/bin/env bash
-echo "Inside the script"
 
-echo $GITHUB_RUN_ID
+echo "setting up log url and branch name"
 
-
-RUN_ID=$(curl -X  GET \
+RUN_ID=$(curl   GET \
                                     https://api.github.com/repos/swapneshran/realworld/actions/runs \
                                     -H "Authorization: token $GITHUB_TOKEN" \
                                     -H "Accept: application/vnd.github.v3+json" \
                                     | jq -r --arg GITHUB_RUN_ID "GITHUB_RUN_ID"  -c '.workflow_runs[] | select( .id | contains('$GITHUB_RUN_ID')) | .head_sha' \
                                     | sed 's/"//g')
-CHECK_SUITE_ID=$(curl -X  GET \
+CHECK_SUITE_ID=$(curl GET \
                                       https://api.github.com/repos/swapneshran/realworld/commits/$RUN_ID/check-runs \
                                      -H "Authorization: token $GITHUB_TOKEN" \
                                      -H "Accept: application/vnd.github.antiope-preview+json" \
                                       | jq '.check_runs[0].id')
-LOG_URL=("https://github.com/repos/swapneshran/realworld/commit/$RUN_ID/checks/$CHECK_SUITE_ID/logs")
+LOG_URL=("https://github.com/swapneshran/realworld/commit/$RUN_ID/checks/$CHECK_SUITE_ID/logs")
 echo $LOG_URL
 echo "##[set-output name=log_url;]$LOG_URL"
 echo "##[set-output name=branch;]$BRANCH_NAME"
